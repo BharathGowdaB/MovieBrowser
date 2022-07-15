261bb4cb87a6f545d254ed6c8ac682e183da2dfd
Bharath Gowda B <bharathvishnubharath@gmail.com>
2022-07-15 22:37:48 +0530

    v3.0

--- app/src/main/java/com/project/moviebrowser/activities/SettingActivity.java
+++ app/src/main/java/com/project/moviebrowser/activities/SettingActivity.java
@@ -1,29 +1,18 @@
 package com.project.moviebrowser.activities;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.project.moviebrowser.R;
-import com.project.moviebrowser.notification.NotificationDailyReceiver;
-import com.project.moviebrowser.notification.NotificationReleaseReceiver;
-import com.project.moviebrowser.preference.SettingPreference;
 
 public class SettingActivity extends AppCompatActivity {
 
     private Switch switchReminder;
     private Switch switchRelease;
-    private NotificationDailyReceiver notificationDailyReceiver;
-    private NotificationReleaseReceiver notificationReleaseReceiver;
-    private SettingPreference settingPreference;
-    private Button button;
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,32 +22,14 @@ public class SettingActivity extends AppCompatActivity {
         switchReminder = findViewById(R.id.swDailyReminder);
         switchRelease = findViewById(R.id.swRealeseToday);
 
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("");
-        setSupportActionBar(toolbar);
-        assert getSupportActionBar() != null;
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        notificationDailyReceiver = new NotificationDailyReceiver();
-        notificationReleaseReceiver = new NotificationReleaseReceiver();
-
-        settingPreference = new SettingPreference(this);
-
-        setSwitchRelease();
-        setSwitchReminder();
-
         // Switch Reminder OnClick
         switchReminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (switchReminder.isChecked()) {
-                    notificationDailyReceiver.setDailyAlarm(getApplicationContext());
-                    settingPreference.setDailyReminder(true);
-                    Toast.makeText(getApplicationContext(), "Daily reminders activated", Toast.LENGTH_SHORT).show();
+                   Toast.makeText(getApplicationContext(), "Daily reminders activated", Toast.LENGTH_SHORT).show();
                 } else {
-                    notificationDailyReceiver.cancelAlarm(getApplicationContext());
-                    settingPreference.setDailyReminder(false);
-                    Toast.makeText(getApplicationContext(), "Daily reminders not activated", Toast.LENGTH_SHORT).show();
+                   Toast.makeText(getApplicationContext(), "Daily reminders not activated", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -68,13 +39,9 @@ public class SettingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (switchRelease.isChecked()) {
-                    notificationReleaseReceiver.setReleaseAlarm(getApplicationContext());
-                    settingPreference.setReleaseReminder(true);
-                    Toast.makeText(getApplicationContext(), "Release reminder activated", Toast.LENGTH_SHORT).show();
+                   Toast.makeText(getApplicationContext(), "Release reminder activated", Toast.LENGTH_SHORT).show();
                 } else {
-                    notificationReleaseReceiver.cancelAlarm(getApplicationContext());
-                    settingPreference.setReleaseReminder(false);
-                    Toast.makeText(getApplicationContext(), "Release reminder not activated", Toast.LENGTH_SHORT).show();
+                   Toast.makeText(getApplicationContext(), "Release reminder not activated", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -82,22 +49,4 @@ public class SettingActivity extends AppCompatActivity {
 
     }
 
-    private void setSwitchReminder() {
-        if (settingPreference.getDailyReminder()) switchReminder.setChecked(true);
-        else switchReminder.setChecked(false);
-    }
-
-    private void setSwitchRelease() {
-        if (settingPreference.getReleaseReminder()) switchRelease.setChecked(true);
-        else switchRelease.setChecked(false);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            this.finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
